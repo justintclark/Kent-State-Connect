70a6a445d6cc910617a3d5b2193badafcc36477f
jtwining21 <72050046+jtwining21@users.noreply.github.com>
2021-03-17 18:45:13 -0400

    Fixed user Register and Login, working on Confirmation Emails

--- KentStateConnect/views.py
+++ KentStateConnect/views.py
@@ -3,11 +3,15 @@ Routes and views for the flask application.
 """
 
 from datetime import datetime
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
-import re
+import re , os
 from KentStateConnect import app
+from itsdangerous import URLSafeTimedSerializer
+from KentStateConnect.email import send_email
+
+
 
 @app.route('/')
 @app.route('/home')
@@ -59,13 +63,32 @@ def tutors():
         message='Due to the ongoing pandemic, tutoring will only be held online through Blackboard Collab between 9am to 9pm EST.'
     )
 
-  
+#main config
+app.secret_key = '12345'  
+SECURITY_PASSWORD_SALT = 'my_precious_two'
+DEBUG = False
+BCRYPT_LOG_ROUNDS = 13
+WTF_CSRF_ENABLED = True
+DEBUG_TB_ENABLED = False
+DEBUG_TB_INTERCEPT_REDIRECTS = False
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'ksc'
-  
 mysql = MySQL(app) 
+
+# mail settings
+MAIL_SERVER = 'smtp.googlemail.com'
+MAIL_PORT = 465
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
+
+# gmail authentication
+MAIL_USERNAME = os.environ['APP_MAIL_USERNAME']
+MAIL_PASSWORD = os.environ['APP_MAIL_PASSWORD']
+
+# mail accounts
+MAIL_DEFAULT_SENDER = 'from@example.com'
   
 @app.route('/') 
 @app.route('/login', methods =['GET', 'POST']) 
@@ -79,7 +102,7 @@ def login():
         account = cursor.fetchone() 
         if account: 
             session['loggedin'] = True
-            session['id'] = account['id'] 
+            session['user_id'] = account['user_id'] 
             session['username'] = account['username'] 
             msg = 'Logged in successfully !'
             return render_template('index.html', msg = msg) 
@@ -101,6 +124,8 @@ def register():
         username = request.form['username'] 
         user_pass = request.form['password'] 
         user_email = request.form['email'] 
+        now = datetime.now()
+        formatted_date = now.strftime('%Y-%m-%d %H:%M:%S')
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor) 
         cursor.execute('SELECT * FROM users WHERE username = % s', (username, )) 
         account = cursor.fetchone() 
@@ -113,9 +138,61 @@ def register():
         elif not username or not user_pass or not user_email: 
             msg = 'Registration failed.'
         else: 
-            cursor.execute('INSERT INTO users VALUES (NULL, % s, % s, % s)', (username, user_pass, user_email, )) 
+            cursor.execute('INSERT INTO users VALUES (NULL, % s, % s, % s, % s, FALSE, NULL, NULL)', (username, user_pass, user_email, formatted_date)) 
             mysql.connection.commit() 
+            token = generate_confirmation_token(user.email)
             msg = 'You have successfully registered !'
+            confirm_url = url_for('confirm_email', token=token, _external=True)
+            html = render_template('activate.html', confirm_url=confirm_url)
+            subject = "Please confirm your email"
+            send_email(user_email, subject, html)
+            flash('A confirmation email has been sent via email.', 'success')
+
     elif request.method == 'POST': 
         msg = 'Registration failed.'
     return render_template('register.html', msg = msg) 
+
+
+
+@app.route('/confirmation', methods =['GET', 'POST']) 
+def generate_confirmation_token(user_email): 
+    msg = '' 
+    if request.method == 'POST' and 'user_email' in request.form: 
+        user_email = request.form['user_email'] 
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor) 
+        serializer = URLSafeTimedSerializer(app.config['secret_key'])
+    return serializer.dumps(user_email, salt=app.config['SECURITY_PASSWORD_SALT'])
+
+def confirm_token(token, expiration=3600):
+    serializer = URLSafeTimedSerializer(app.config['secret_key'])
+    if request.method == 'POST' and 'user_email' in request.form: 
+        user_email = request.form['user_email'] 
+    try:
+        user_email = serializer.loads(
+            token,
+            salt=app.config['SECURITY_PASSWORD_SALT'],
+            max_age=expiration
+        )
+    except:
+        return False
+    return user_email
+
+@app.route('/confirm/<token>')
+def confirm_email(token):
+    if request.method == 'POST' and 'user_email' and 'user_confirmed' and 'user_confirmed_on' in request.form: 
+        user_email = request.form['user_email'] 
+        user_confirmed = request.form['user_confirmed']
+        user_confirmed_on = request.form['user_confirmed_on']
+    try:
+        user_email = confirm_token(token)
+    except:
+        flash('The confirmation link is invalid or has expired.', 'danger')
+    user = User.query.filter_by(email=email).first_or_404()
+    if user.confirmed:
+        flash('Account already confirmed. Please login.', 'success')
+    else:
+        user_confirmed = True
+        user_confirmed_on = datetime.datetime.now()
+        mysql.connection.commit() 
+        flash('You have confirmed your account. Thanks!', 'success')
+    return redirect(url_for('home'))
