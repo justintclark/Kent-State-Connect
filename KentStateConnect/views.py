27777326145e789e186588f11034aac398a2dfd2
justintclark <55370053+justintclark@users.noreply.github.com>
2021-03-24 17:09:40 -0400

    Files added for forum structure
    Still getting errors with the setup file data

--- KentStateConnect/views.py
+++ KentStateConnect/views.py
@@ -5,6 +5,9 @@ Routes and views for the flask application.
 from datetime import datetime
 from flask import render_template
 from KentStateConnect import app
+from django.shortcuts import render,redirect
+from .models import * 
+from .forms import * 
 
 @app.route('/')
 @app.route('/home')
@@ -48,13 +51,16 @@ def resources():
 
 @app.route('/forum')
 def forum():
-    """Renders the Forum page."""
-    return render_template(
-        'forum.html',
-        title='Forum',
-        year=datetime.now().year,
-        message='The forum is currently under development.'
-    )
+    forums=forum.objects.all()
+    count=forums.count()
+    discussions=[]
+    for i in forums:
+        discussions.append(i.discussion_set.all())
+
+    context={'forums':forums,
+              'count':count,
+              'discussions':discussions}
+    return render(request,'forum.html',context)
 
 @app.route('/tutors')
 def tutors():
@@ -65,3 +71,23 @@ def tutors():
         year=datetime.now().year,
         message='Due to the ongoing pandemic, tutoring will only be held online through Blackboard Collab between 9am to 9pm EST.'
     )
+
+def addInForum(request):
+    form = CreateInForum()
+    if request.method == 'POST':
+        form = CreateInForum(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    context ={'form':form}
+    return render(request,'addInForum.html',context)
+
+def addInDiscussion(request):
+    form = CreateInDiscussion()
+    if request.method == 'POST':
+        form = CreateInDiscussion(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    context ={'form':form}
+    return render(request,'addInDiscussion.html',context)
