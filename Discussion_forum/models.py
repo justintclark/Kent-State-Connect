efc102e1c8e7a7bcd8df8d1c17023e98f707707a
justintclark <55370053+justintclark@users.noreply.github.com>
2021-04-07 14:50:08 -0400

    Visual changes to the site

--- Discussion_forum/models.py
+++ Discussion_forum/models.py
@@ -3,10 +3,10 @@ from django.db import models
     
 #parent model
 class forum(models.Model):
-    name=models.CharField(max_length=200,default="anonymous" )
-    email=models.CharField(max_length=200,null=True)
+    name=models.CharField(max_length=200)
+    email=models.CharField(max_length=200,null=False)
     topic= models.CharField(max_length=300)
-    description = models.CharField(max_length=10000,blank=True)
+    description = models.CharField(max_length=10000,blank=False)
     #link = models.CharField(max_length=100 ,null =True)
     date_created=models.DateTimeField(auto_now_add=True,null=True)
     
@@ -16,7 +16,7 @@ class forum(models.Model):
 #child model
 class Discussion(models.Model):
     forum = models.ForeignKey(forum,blank=True,on_delete=models.CASCADE)
-    name=models.CharField(max_length=200,default="anonymous" )
+    name=models.CharField(max_length=200)
     discuss = models.CharField(max_length=10000)
 
     def __str__(self):
