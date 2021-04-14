330d39bd20a5a7041369dd698727353826a4680b
justintclark <55370053+justintclark@users.noreply.github.com>
2021-04-14 13:12:05 -0400

    Eliminated "email" from post creation, since it wasn't used.

--- Discussion_forum/migrations/0001_initial.py
+++ Discussion_forum/migrations/0001_initial.py
@@ -32,7 +32,6 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(default='anonymous', max_length=200)),
-                ('email', models.CharField(max_length=200, null=True)),
                 ('title', models.CharField(max_length=300)),
                 ('description', models.CharField(blank=True, max_length=1000)),
                 ('date_created', models.DateTimeField(auto_now_add=True, null=True)),
