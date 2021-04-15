4eedb2168ad462cbb87d0fb346a77a733fe0999b
jtwining21 <72050046+jtwining21@users.noreply.github.com>
2021-04-14 20:46:39 -0400

    Custom 404 and 500 error pages
    Custom 404 and 500 error pages
    404.html
    500.html
    functions added to bottom of views.py

--- KentStateConnect/views.py
+++ KentStateConnect/views.py
@@ -361,3 +361,17 @@ def resetpassword(email, code):
 				msg = 'Passwords must match and must not be empty!'
 		return render_template('resetpassword.html', msg=msg, email=email, code=code)
 	return 'Invalid email and/or code!'
+
+
+
+# Custom Error Pages
+
+# Invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'), 404
+
+# Internal Server Error
+@app.errorhandler(500)
+def page_not_found(e):
+	return render_template('500.html'), 500
